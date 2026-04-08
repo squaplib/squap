cb88ee62f8a044bee3ba8d32870f5e78a046d62f
SlikRik <rikmulder7@gmail.com>
2026-04-08 16:38:33 +0200

    added getitem, setitem and delitem functionality to Variables class

--- squap/variables.py
+++ squap/variables.py
@@ -9,11 +9,20 @@ class Variables:
         else:
             raise AttributeError(f"'Namespace' object has no attribute '{name}'")
 
+    def __getitem__(self, key):
+        return self.__getattr__(key)
+
     def __setattr__(self, name, value):
         if name != '_variables':
             self._variables[name] = value
         else:
             raise ValueError(f"'_variables' is a reserved name and cannot be used")
+
+    def __setitem__(self, key, value):
+        self.__setattr__(key, value)
+
+    def __delitem__(self, key):
+        del self._variables[key]
 
     def __repr__(self):
         result = "Variables:\n"
