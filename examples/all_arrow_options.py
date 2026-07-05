34d67f36a14e49ea419d38e5e0994e34989d373f
SlikRik <rikmulder7@gmail.com>
2026-07-06 01:07:10 +0200

    added examples for arrows and vector fields.

---
+++ examples/all_arrow_options.py
@@ -0,0 +1,92 @@
+"""
+Arrow Customisation
+==================
+
+Shows all arrow customisation options.
+"""
+import squap
+from squap import var
+import numpy as np
+
+
+var.trigger = 0
+
+
+def update():
+    kwargs = {
+        "angle": var.angle, "size": var.size, "position": var.position, "head_length": var.head_length, "base_angle": var.base_angle,
+        "tail_length": var.tail_length, "tail_width": var.tail_width, "pixel_mode": var.pixel_mode, "origin": var.origin,
+        "fill_color": var.fill_color, "border_color": var.border_color, "border_width": var.border_width, "length": var.length}
+    if var.tip_style == "tip_angle":
+        pass
+        # kwargs["tip_angle"] = var.tip_angle
+    else:
+        kwargs["head_width"] = var.head_width
+    arrow.set_data(
+        **kwargs
+    )
+
+
+def update_dropdown():
+    boxes[7].change_params(name=var.tip_style)
+
+
+def pixel_mode_off():
+    boxes[5].set_value(0.1)
+    boxes[9].set_value(0.25)
+    boxes[10].set_value(0.03)
+    boxes[11].set_value(False)
+
+
+def resize_params(flag=False):
+    data = arrow.get_data()
+    for box_index, param in zip([9, 10, 5, 7], ["tail_length", "tail_width", "head_length", "head_width"]):
+        if var.tip_style == "tip_angle" and param == "head_width":
+            if flag:
+                boxes[box_index].set_value(arrow.get_data()["tip_angle"])
+                continue
+            else:
+                continue
+
+        boxes[box_index].set_value(data[param])
+
+
+def test_button():
+    arrow.setStyle(angle=45)
+
+
+boxes = [
+    squap.add_slider("angle", 0, 0, 360),
+    squap.add_inputbox("angle", 0.),
+    squap.add_inputbox("size", 1),
+    squap.add_inputbox("position", (0., 0.)),
+    squap.add_dropdown("origin", ["tip", "center", "tail"]),
+    squap.add_inputbox("head_length", 20),
+    squap.add_dropdown("tip_style", ["tip_angle", "head_width"], 0).bind(update_dropdown),
+    squap.add_inputbox("tip_angle", 60),
+    squap.add_inputbox("base_angle", 0),
+    squap.add_inputbox("tail_length", 50),
+    squap.add_inputbox("tail_width", 6),
+    squap.add_checkbox("pixel_mode", True),
+    squap.add_color_picker("fill_color", "white"),
+    squap.add_color_picker("border_color", "blue"),
+    squap.add_inputbox("border_width", -1),
+    squap.add_button("test", test_button),
+    squap.add_inputbox("length", 1),
+]
+squap.add_button("change to coordinate mode", pixel_mode_off)
+
+for box in boxes:
+    box.bind(update)
+
+boxes[2].bind(resize_params)        # should be called after update
+boxes[-1].bind(lambda: resize_params(True))
+
+squap.link_boxes(boxes[0:2])
+var.head_width = 10
+
+x = np.linspace(-1, 1, 100)
+squap.plot(np.sin(2*np.pi*x), np.cos(2*np.pi*x))
+arrow = squap.arrow()
+update()
+squap.show()
---
+++ examples/all_vector_field_options.py
@@ -0,0 +1,120 @@
+"""
+Vector Field Customisation
+==================
+
+Shows all vector field customisation options.
+Not thoroughly tested.
+"""
+import squap
+from squap import var
+import numpy as np
+
+
+def update(pos=None):
+    if pos is None:
+        positions = var.positions
+        q_values = var.q_values
+    else:
+        positions = var.positions + [pos]
+        q_values = var.q_values + [var.q]
+
+    centres = np.array(positions)
+    # centre = pos
+    F[:] = 0
+
+    for index, centre in enumerate(centres):
+        x_rel = x_grid - centre[0]
+        y_rel = y_grid - centre[1]
+        theta = np.arctan2(y_rel, x_rel)
+        r = np.sqrt(x_rel**2 + y_rel**2)
+        F[:, :, 0] -= q_values[index]*np.cos(theta)/r**var.power
+        F[:, :, 1] -= q_values[index]*np.sin(theta)/r**var.power
+
+    curve.set_data(F, border_width=-1, )
+
+
+def add_atom(pos):
+    var.positions.append(pos)
+    var.q_values.append(var.q)
+    if var.q < 0:
+        squap.scatter([pos[0]], [pos[1]], color="red")
+    else:
+        squap.scatter([pos[0]], [pos[1]], color="blue")
+
+
+def update_kwargs():
+    kwargs = {
+        "scale_type": var.scale_type, "head_length": var.head_length, "base_angle": var.base_angle,
+        "tail_length": var.tail_length, "tail_width": var.tail_width, "pixel_mode": var.pixel_mode,
+        "fill_color": var.fill_color, "border_color": var.border_color, "border_width": var.border_width}
+    if var.tip_style == "tip_angle":
+        kwargs["tip_angle"] = var.tip_angle
+    else:
+        kwargs["head_width"] = var.head_width
+    curve.set_data(
+        **kwargs
+        )
+
+
+def update_dropdown():
+    boxes[7].change_params(name=var.tip_style)
+
+
+def pixel_mode_off():
+    boxes[0].set_value(0.005)
+    boxes[4].set_value(0.0125)
+    boxes[5].set_value(0.0015)
+    boxes[6].set_value(False)
+
+
+def resize_params():
+    data = arrow.get_data()
+    for box_index, param in zip([9, 10, 5, 7], ["tail_length", "tail_width", "head_length", "head_width"]):
+        if var.tip_style == "tip_angle" and param == "head_width":
+            continue
+        boxes[box_index].set_value(data[param])
+
+
+min_co, max_co = -0.1, 0.1
+var.positions = []
+var.q_values = []
+Nx, Ny = 15, 20
+x = np.linspace(min_co, max_co, Nx)
+y = np.linspace(min_co, max_co, Ny)
+x_grid, y_grid = np.meshgrid(x, y)
+
+F = np.zeros((Ny, Nx, 2))
+
+squap.on_mouse_move(update)
+squap.on_mouse_leave(update)
+squap.on_mouse_click(add_atom)
+
+tab1 = squap.add_tab("data")
+tab2 = squap.add_tab("vector field properties")
+
+tab1.add_inputbox("q", -1).bind(update)
+tab1.add_inputbox("power", 2).bind(update)
+
+boxes = [
+    tab2.add_dropdown("scale_type", ["None", "length", "size", "color"], 0),
+    tab2.add_inputbox("head_length", 4),
+    tab2.add_dropdown("tip_style", ["tip_angle", "head_width"], 0).bind(update_dropdown),
+    tab2.add_inputbox("tip_angle", 60),
+    tab2.add_inputbox("base_angle", 0),
+    tab2.add_inputbox("tail_length", 10),
+    tab2.add_inputbox("tail_width", 1.2),
+    tab2.add_checkbox("pixel_mode", True),
+    tab2.add_color_picker("fill_color", "yellow"),
+    tab2.add_color_picker("border_color", "blue"),
+    tab2.add_inputbox("border_width", -1),
+]
+tab2.add_button("change to coordinate mode", pixel_mode_off)
+
+for box in boxes:
+    box.bind(update_kwargs)
+
+curve = squap.vector_field(data=F, pos_x=x_grid, pos_y=y_grid)
+squap.set_xlim(min_co, max_co)
+squap.set_ylim(min_co, max_co)
+update((0, 0))
+squap.show()
